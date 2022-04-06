dc5881f1405aed8247ac9e762123a1aae1009ae5
Camila Abreu <camila.abreu@grupofullsix.pt>
2022-04-06 16:14:49 +0100

    ADD: testimonials

--- main.js
+++ main.js
@@ -4,22 +4,36 @@ const links = document.querySelectorAll('nav ul li a');
 const header = document.querySelector("#header");
 const navHeight = header.offsetHeight;
 
+/* Open and Close menu hamburguer */
 for(const element of toggle) {
   element.addEventListener('click', function() {
     nav.classList.toggle('show')
   })
 }
 
+/* Close menu quando carregar em algum item do menu */
 for(const link of links) {
   link.addEventListener('click', function() {
     nav.classList.remove('show')
   })
 }
 
+/* Mudar header dá pagina com scroll */
 window.addEventListener('scroll', function() {
   if(this.window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
+})
+
+/* Testimonials */
+
+const swiper = new Swiper ('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
 })
