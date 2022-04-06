e035a12eda4e2aadc8edb42a20776eb6152c1d60
Camila Abreu <camila.abreu@grupofullsix.pt>
2022-04-06 19:27:41 +0100

    ADD: contact and scroll reveal

--- main.js
+++ main.js
@@ -37,3 +37,22 @@ const swiper = new Swiper ('.swiper-container', {
   mousewheel: true,
   keyboard: true
 })
+
+/* ScrollReveal */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+`, 
+  {interval: 100}
+)
