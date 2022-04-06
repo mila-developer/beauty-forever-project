ed30f8696fa1daa703b994a49f53966cf8fe1891
Camila Abreu <camila.abreu@grupofullsix.pt>
2022-04-06 14:32:51 +0100

    UPD: header scroll

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
-const links = document.querySelectorAll('nav ul li a')
-
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
+const links = document.querySelectorAll('nav ul li a');
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 
 for(const element of toggle) {
   element.addEventListener('click', function() {
@@ -14,3 +15,11 @@ for(const link of links) {
     nav.classList.remove('show')
   })
 }
+
+window.addEventListener('scroll', function() {
+  if(this.window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
