92ee9d695ecb3688e93e209f178d09bca80da144
Camila Abreu <camila.abreu@grupofullsix.pt>
2022-04-06 20:39:34 +0100

    UPD: icon and ADD: button back-to-top

--- main.js
+++ main.js
@@ -19,13 +19,13 @@ for(const link of links) {
 }
 
 /* Mudar header dá pagina com scroll */
-window.addEventListener('scroll', function() {
+function changeHeaderWhenScroll() {
   if(this.window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonials */
 
@@ -52,7 +52,24 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  #footer .brand, #footer .social
 `, 
   {interval: 100}
 )
+
+/* Button back-to-top */
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if(this.window.scrollY >= 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function() {
+  changeHeaderWhenScroll()
+  backToTop()
+ })
